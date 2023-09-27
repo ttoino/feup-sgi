89164844770db0aeae7cb06fa17c270c445bb160
Nuno Pereira <nunoafonso2002@gmail.com>
2023-09-27 10:43:00 +0100

    formatted code

--- tp1/MyAxis.js
+++ tp1/MyAxis.js
@@ -26,32 +26,32 @@ class MyAxis extends THREE.Object3D {
         this.zzColor = zzColor || 0x0000ff
 
         // a cone geometry for the xx axis
-        const xx = new THREE.ConeGeometry( this.baseRadius, this.size, 32 ); 
-        const xxMaterial = new THREE.MeshBasicMaterial( {color: this.xxColor} );
-        const xxMesh = new THREE.Mesh(xx, xxMaterial ); 
-        xxMesh.position.set(this.size/2,0,0);
+        const xx = new THREE.ConeGeometry(this.baseRadius, this.size, 32);
+        const xxMaterial = new THREE.MeshBasicMaterial({ color: this.xxColor });
+        const xxMesh = new THREE.Mesh(xx, xxMaterial);
+        xxMesh.position.set(this.size / 2, 0, 0);
         xxMesh.rotation.z = -Math.PI / 2;
-        this.add( xxMesh );
+        this.add(xxMesh);
 
         // a cone geometry for the yy axis
-        const yy = new THREE.ConeGeometry( this.baseRadius, this.size, 32 ); 
-        const yyMaterial = new THREE.MeshBasicMaterial( {color: this.yyColor} );
-        const yyMesh = new THREE.Mesh(yy, yyMaterial ); 
-        yyMesh.position.set(0, this.size/2,0);
-        this.add( yyMesh );
+        const yy = new THREE.ConeGeometry(this.baseRadius, this.size, 32);
+        const yyMaterial = new THREE.MeshBasicMaterial({ color: this.yyColor });
+        const yyMesh = new THREE.Mesh(yy, yyMaterial);
+        yyMesh.position.set(0, this.size / 2, 0);
+        this.add(yyMesh);
 
         // a cone geometry for the zz axis
-        const zz = new THREE.ConeGeometry( this.baseRadius, this.size, 32 ); 
-        const zzMaterial = new THREE.MeshBasicMaterial( {color: this.zzColor} );
-        const zzMesh = new THREE.Mesh(zz, zzMaterial ); 
-        zzMesh.position.set(0,0,this.size/2);
+        const zz = new THREE.ConeGeometry(this.baseRadius, this.size, 32);
+        const zzMaterial = new THREE.MeshBasicMaterial({ color: this.zzColor });
+        const zzMesh = new THREE.Mesh(zz, zzMaterial);
+        zzMesh.position.set(0, 0, this.size / 2);
         zzMesh.rotation.x = Math.PI / 2;
-        this.add( zzMesh ); 
+        this.add(zzMesh);
 
         // an axis helper
-        const axesHelper = new THREE.AxesHelper( 5 );
-        axesHelper.setColors ( new THREE.Color( this.xxColor ),  new THREE.Color( this.yyColor ),  new THREE.Color( this.zzColor ))
-        this.add( axesHelper );
+        const axesHelper = new THREE.AxesHelper(5);
+        axesHelper.setColors(new THREE.Color(this.xxColor), new THREE.Color(this.yyColor), new THREE.Color(this.zzColor))
+        this.add(axesHelper);
     }
 }
 
--- tp1/MyContents.js
+++ tp1/MyContents.js
@@ -4,12 +4,12 @@ import { MyAxis } from './MyAxis.js';
 /**
  *  This class contains the contents of out application
  */
-class MyContents  {
+class MyContents {
 
     /**
        constructs the object
        @param {MyApp} app The application object
-    */ 
+    */
     constructor(app) {
         this.app = app
         this.axis = null
@@ -19,27 +19,31 @@ class MyContents  {
         this.boxMeshSize = 1.0
         this.boxEnabled = true
         this.lastBoxEnabled = null
-        this.boxDisplacement = new THREE.Vector3(0,2,0)
+        this.boxDisplacement = new THREE.Vector3(0, 2, 0)
 
         // plane related attributes
         this.diffusePlaneColor = "#00ffff"
         this.specularPlaneColor = "#777777"
         this.planeShininess = 30
-        this.planeMaterial = new THREE.MeshPhongMaterial({ color: this.diffusePlaneColor, 
-            specular: this.diffusePlaneColor, emissive: "#000000", shininess: this.planeShininess })
+        this.planeMaterial = new THREE.MeshPhongMaterial({
+            color: this.diffusePlaneColor,
+            specular: this.diffusePlaneColor, emissive: "#000000", shininess: this.planeShininess
+        })
     }
 
     /**
      * builds the box mesh with material assigned
      */
-    buildBox() {    
-        let boxMaterial = new THREE.MeshPhongMaterial({ color: "#ffff77", 
-        specular: "#000000", emissive: "#000000", shininess: 90 })
+    buildBox() {
+        let boxMaterial = new THREE.MeshPhongMaterial({
+            color: "#ffff77",
+            specular: "#000000", emissive: "#000000", shininess: 90
+        })
 
         // Create a Cube Mesh with basic material
-        let box = new THREE.BoxGeometry(  this.boxMeshSize,  this.boxMeshSize,  this.boxMeshSize );
-        this.boxMesh = new THREE.Mesh( box, boxMaterial );
-        this.boxMesh.rotation.x = -Math.PI / 2;
+        let box = new THREE.BoxGeometry(this.boxMeshSize, this.boxMeshSize, this.boxMeshSize);
+        this.boxMesh = new THREE.Mesh(box, boxMaterial);
+        this.boxMesh.rotation.x = -Math.PI / 3;
         this.boxMesh.position.y = this.boxDisplacement.y;
     }
 
@@ -47,7 +51,7 @@ class MyContents  {
      * initializes the contents
      */
     init() {
-       
+
         // create once 
         if (this.axis === null) {
             // create and attach the axis to the scene
@@ -56,30 +60,30 @@ class MyContents  {
         }
 
         // add a point light on top of the model
-        const pointLight = new THREE.PointLight( 0xffffff, 500, 0 );
-        pointLight.position.set( 0, 20, 0 );
-        this.app.scene.add( pointLight );
+        const pointLight = new THREE.PointLight(0xffffff, 500, 0);
+        pointLight.position.set(0, 20, 0);
+        this.app.scene.add(pointLight);
 
         // add a point light helper for the previous point light
         const sphereSize = 0.5;
-        const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
-        this.app.scene.add( pointLightHelper );
+        const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
+        this.app.scene.add(pointLightHelper);
 
         // add an ambient light
-        const ambientLight = new THREE.AmbientLight( 0x555555 );
-        this.app.scene.add( ambientLight );
+        const ambientLight = new THREE.AmbientLight(0x555555);
+        this.app.scene.add(ambientLight);
 
         this.buildBox()
-        
+
         // Create a Plane Mesh with basic material
-        
-        let plane = new THREE.PlaneGeometry( 10, 10 );
-        this.planeMesh = new THREE.Mesh( plane, this.planeMaterial );
+
+        let plane = new THREE.PlaneGeometry(10, 10);
+        this.planeMesh = new THREE.Mesh(plane, this.planeMaterial);
         this.planeMesh.rotation.x = -Math.PI / 2;
         this.planeMesh.position.y = -0;
-        this.app.scene.add( this.planeMesh );
+        this.app.scene.add(this.planeMesh);
     }
-    
+
     /**
      * updates the diffuse plane color and the material
      * @param {THREE.Color} value 
@@ -104,20 +108,20 @@ class MyContents  {
         this.planeShininess = value
         this.planeMaterial.shininess = this.planeShininess
     }
-    
+
     /**
      * rebuilds the box mesh if required
      * this method is called from the gui interface
      */
     rebuildBox() {
         // remove boxMesh if exists
-        if (this.boxMesh !== undefined && this.boxMesh !== null) {  
+        if (this.boxMesh !== undefined && this.boxMesh !== null) {
             this.app.scene.remove(this.boxMesh)
         }
         this.buildBox();
         this.lastBoxEnabled = null
     }
-    
+
     /**
      * updates the box mesh if required
      * this method is called from the render method of the app
@@ -148,7 +152,7 @@ class MyContents  {
         this.boxMesh.position.x = this.boxDisplacement.x
         this.boxMesh.position.y = this.boxDisplacement.y
         this.boxMesh.position.z = this.boxDisplacement.z
-        
+
     }
 
 }
