c6bc7070293f7fec5a323529a5c3bc80ffbce6a1
Nuno Pereira <nunoafonso2002@gmail.com>
2023-10-04 11:06:50 +0100

    Added different lights

--- tp1/MyContents.js
+++ tp1/MyContents.js
@@ -4,12 +4,12 @@ import { MyAxis } from './MyAxis.js';
 /**
  *  This class contains the contents of out application
  */
-class MyContents  {
+class MyContents {
 
     /**
        constructs the object
        @param {MyApp} app The application object
-    */ 
+    */
     constructor(app) {
         this.app = app
         this.axis = null
@@ -19,26 +19,39 @@ class MyContents  {
         this.boxMeshSize = 1.0
         this.boxEnabled = true
         this.lastBoxEnabled = null
-        this.boxDisplacement = new THREE.Vector3(0,2,0)
+        this.boxDisplacement = new THREE.Vector3(0, 2, 0)
 
         // plane related attributes
         this.diffusePlaneColor = "#00ffff"
         this.specularPlaneColor = "#777777"
         this.planeShininess = 30
-        this.planeMaterial = new THREE.MeshPhongMaterial({ color: this.diffusePlaneColor, 
-            specular: this.diffusePlaneColor, emissive: "#000000", shininess: this.planeShininess })
+        this.planeMaterial = new THREE.MeshPhongMaterial({
+            color: this.diffusePlaneColor,
+            specular: this.diffusePlaneColor, emissive: "#000000", shininess: this.planeShininess
+        });
+
+        // spotligh related attributes
+        this.spotLightColor = "#ffffff";
+        this.spotLightIntensity = 15;
+        this.spotLightDistance = 8;
+        this.spotLightAngle = 2 * Math.PI / 9;
+        this.spotLightPenumbra = 0;
+        this.spotLightDecay = 0;
+        this.spotLightTargetY = 0;
     }
 
     /**
      * builds the box mesh with material assigned
      */
-    buildBox() {    
-        let boxMaterial = new THREE.MeshPhongMaterial({ color: "#ffff77", 
-        specular: "#000000", emissive: "#000000", shininess: 90 })
+    buildBox() {
+        let boxMaterial = new THREE.MeshPhongMaterial({
+            color: "#ffff77",
+            specular: "#000000", emissive: "#000000", shininess: 90
+        })
 
         // Create a Cube Mesh with basic material
-        let box = new THREE.BoxGeometry(  this.boxMeshSize,  this.boxMeshSize,  this.boxMeshSize );
-        this.boxMesh = new THREE.Mesh( box, boxMaterial );
+        let box = new THREE.BoxGeometry(this.boxMeshSize, this.boxMeshSize, this.boxMeshSize);
+        this.boxMesh = new THREE.Mesh(box, boxMaterial);
         this.boxMesh.rotation.x = -Math.PI / 2;
         this.boxMesh.position.y = this.boxDisplacement.y;
     }
@@ -47,7 +60,7 @@ class MyContents  {
      * initializes the contents
      */
     init() {
-       
+
         // create once 
         if (this.axis === null) {
             // create and attach the axis to the scene
@@ -56,30 +69,49 @@ class MyContents  {
         }
 
         // add a point light on top of the model
-        const pointLight = new THREE.PointLight( 0xffffff, 500, 0 );
-        pointLight.position.set( 0, 20, 0 );
-        this.app.scene.add( pointLight );
+        // const pointLight = new THREE.PointLight( 0xffffff, 500, 0 );
+        // pointLight.position.set( 0, 20, 0 );
+        // this.app.scene.add( pointLight );
 
         // add a point light helper for the previous point light
-        const sphereSize = 0.5;
-        const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
-        this.app.scene.add( pointLightHelper );
+        // const sphereSize = 0.5;
+        // const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
+        // this.app.scene.add( pointLightHelper );
+
+        // this.light2 = new THREE.DirectionalLight(0xffffff, 3);
+        // this.light2.position.set(-5, 10, -2);
+        // if (this.boxMesh)
+        //     this.light2.target = this.boxMesh;
+
+        // this.app.scene.add(this.light2);
+
+        // const directionalLightHelper = new THREE.DirectionalLightHelper(this.light2);
+        // this.app.scene.add(directionalLightHelper);
+
+        this.spotLight = new THREE.SpotLight(this.spotLightColor, this.spotLightIntensity, this.spotLightDistance, this.spotLightAngle, this.spotLightPenumbra, this.spotLightDecay);
+        this.spotLight.position.set(2, 5, 1);
+        this.spotLight.target.position.set(1, this.spotLightTargetY, 1);
+
+        this.app.scene.add(this.spotLight);
+
+        this.spotLightHelper = new THREE.SpotLightHelper(this.spotLight);
+        this.app.scene.add(this.spotLightHelper);
 
         // add an ambient light
-        const ambientLight = new THREE.AmbientLight( 0x555555 );
-        this.app.scene.add( ambientLight );
+        const ambientLight = new THREE.AmbientLight(0x555555, 4);
+        this.app.scene.add(ambientLight);
 
         this.buildBox()
-        
+
         // Create a Plane Mesh with basic material
-        
-        let plane = new THREE.PlaneGeometry( 10, 10 );
-        this.planeMesh = new THREE.Mesh( plane, this.planeMaterial );
+
+        let plane = new THREE.PlaneGeometry(10, 10);
+        this.planeMesh = new THREE.Mesh(plane, this.planeMaterial);
         this.planeMesh.rotation.x = -Math.PI / 2;
         this.planeMesh.position.y = -0;
-        this.app.scene.add( this.planeMesh );
+        this.app.scene.add(this.planeMesh);
     }
-    
+
     /**
      * updates the diffuse plane color and the material
      * @param {THREE.Color} value 
@@ -88,6 +120,16 @@ class MyContents  {
         this.diffusePlaneColor = value
         this.planeMaterial.color.set(this.diffusePlaneColor)
     }
+
+    /**
+    * updates the diffuse plane color and the material
+    * @param {THREE.Color} value 
+    */
+    updateSpotlightColor(value) {
+        this.spotLightColor = value;
+        this.spotLight.color.set(this.spotLightColor);
+    }
+
     /**
      * updates the specular plane color and the material
      * @param {THREE.Color} value 
@@ -104,20 +146,20 @@ class MyContents  {
         this.planeShininess = value
         this.planeMaterial.shininess = this.planeShininess
     }
-    
+
     /**
      * rebuilds the box mesh if required
      * this method is called from the gui interface
      */
     rebuildBox() {
         // remove boxMesh if exists
-        if (this.boxMesh !== undefined && this.boxMesh !== null) {  
+        if (this.boxMesh !== undefined && this.boxMesh !== null) {
             this.app.scene.remove(this.boxMesh)
         }
         this.buildBox();
         this.lastBoxEnabled = null
     }
-    
+
     /**
      * updates the box mesh if required
      * this method is called from the render method of the app
@@ -135,6 +177,20 @@ class MyContents  {
         }
     }
 
+    updateSpotlight() {
+        this.spotLight.angle = this.spotLightAngle;
+        this.spotLight.decay = this.spotLightDecay;
+        this.spotLight.intensity = this.spotLightIntensity;
+        this.spotLight.distance = this.spotLightDistance;
+        this.spotLight.penumbra = this.spotLightPenumbra;
+
+        this.spotLight.target.position.set(1, this.spotLightTargetY, 1);
+
+        this.app.scene.remove(this.spotLightHelper);
+        this.spotLightHelper = new THREE.SpotLightHelper(this.spotLight);
+        this.app.scene.add(this.spotLightHelper);
+    }
+
     /**
      * updates the contents
      * this method is called from the render method of the app
@@ -143,12 +199,12 @@ class MyContents  {
     update() {
         // check if box mesh needs to be updated
         this.updateBoxIfRequired()
+        this.updateSpotlight();
 
         // sets the box mesh position based on the displacement vector
         this.boxMesh.position.x = this.boxDisplacement.x
         this.boxMesh.position.y = this.boxDisplacement.y
         this.boxMesh.position.z = this.boxDisplacement.z
-        
     }
 
 }
--- tp1/MyGuiInterface.js
+++ tp1/MyGuiInterface.js
@@ -5,7 +5,7 @@ import { MyContents } from './MyContents.js';
 /**
     This class customizes the gui interface for the app
 */
-class MyGuiInterface  {
+class MyGuiInterface {
 
     /**
      * 
@@ -13,7 +13,7 @@ class MyGuiInterface  {
      */
     constructor(app) {
         this.app = app
-        this.datgui =  new GUI();
+        this.datgui = new GUI();
         this.contents = null
     }
 
@@ -30,33 +30,48 @@ class MyGuiInterface  {
      */
     init() {
         // add a folder to the gui interface for the box
-        const boxFolder = this.datgui.addFolder( 'Box' );
+        const boxFolder = this.datgui.addFolder('Box');
         // note that we are using a property from the contents object 
-        boxFolder.add(this.contents, 'boxMeshSize', 0, 10).name("size").onChange( () => { this.contents.rebuildBox() } );
+        boxFolder.add(this.contents, 'boxMeshSize', 0, 10).name("size").onChange(() => { this.contents.rebuildBox() });
         boxFolder.add(this.contents, 'boxEnabled', true).name("enabled");
         boxFolder.add(this.contents.boxDisplacement, 'x', -5, 5)
         boxFolder.add(this.contents.boxDisplacement, 'y', -5, 5)
         boxFolder.add(this.contents.boxDisplacement, 'z', -5, 5)
         boxFolder.open()
-        
-        const data = {  
+
+        const data = {
             'diffuse color': this.contents.diffusePlaneColor,
             'specular color': this.contents.specularPlaneColor,
         };
 
         // adds a folder to the gui interface for the plane
-        const planeFolder = this.datgui.addFolder( 'Plane' );
-        planeFolder.addColor( data, 'diffuse color' ).onChange( (value) => { this.contents.updateDiffusePlaneColor(value) } );
-        planeFolder.addColor( data, 'specular color' ).onChange( (value) => { this.contents.updateSpecularPlaneColor(value) } );
-        planeFolder.add(this.contents, 'planeShininess', 0, 1000).name("shininess").onChange( (value) => { this.contents.updatePlaneShininess(value) } );
+        const planeFolder = this.datgui.addFolder('Plane');
+        planeFolder.addColor(data, 'diffuse color').onChange((value) => { this.contents.updateDiffusePlaneColor(value) });
+        planeFolder.addColor(data, 'specular color').onChange((value) => { this.contents.updateSpecularPlaneColor(value) });
+        planeFolder.add(this.contents, 'planeShininess', 0, 1000).name("shininess").onChange((value) => { this.contents.updatePlaneShininess(value) });
         planeFolder.open();
 
         // adds a folder to the gui interface for the camera
         const cameraFolder = this.datgui.addFolder('Camera')
-        cameraFolder.add(this.app, 'activeCameraName', [ 'Perspective', 'Left', 'Top', 'Front' ] ).name("active camera");
+        cameraFolder.add(this.app, 'activeCameraName', ['Perspective', 'Left', 'Top', 'Front']).name("active camera");
         // note that we are using a property from the app 
         cameraFolder.add(this.app.activeCamera.position, 'x', 0, 10).name("x coord")
         cameraFolder.open()
+
+        const spotLightFolder = this.datgui.addFolder('Empty');
+        spotLightFolder.addColor({
+            white: "white",
+            blue: "blue",
+            black: "black",
+            red: "red"
+        }, 'white').onChange((value) => { this.contents.updateSpotlightColor(value); });
+        spotLightFolder.add(this.contents, 'spotLightIntensity', 0, 100).name('spotlight intensity');
+        spotLightFolder.add(this.contents, 'spotLightDistance', 0, 100).name('spotlight distance');
+        spotLightFolder.add(this.contents, 'spotLightAngle', 0, Math.PI).name('spotlight angle');
+        spotLightFolder.add(this.contents, 'spotLightPenumbra', 0, 1).name('spotlight penumbra');
+        spotLightFolder.add(this.contents, 'spotLightDecay', 0, 1).name('spotlight decay');
+        spotLightFolder.add(this.contents, 'spotLightTargetY', -10, 10).name('spotlight target');
+        spotLightFolder.open();
     }
 }
 
