110642e5b7f9457f27292223c5cd27f2fde5e228
João António Semedo Pereira <up202007145@edu.fe.up.pt>
2023-10-25 23:04:53 +0100

    Build scene graph

--- tp2/MyContents.js
+++ tp2/MyContents.js
@@ -3,6 +3,7 @@ import { MyAxis } from "./MyAxis.js";
 import { MyFileReader } from "./parser/MyFileReader.js";
 import { MySceneData } from "./parser/MySceneData.js";
 import { MyApp } from "./MyApp.js";
+import { degreesToRadians } from "./MyMath.js";
 
 /**
  *  This class contains the contents of out application
@@ -133,34 +134,259 @@ class MyContents {
         this.app.setActiveCamera(data.activeCameraId);
 
         console.log("nodes:");
-        for (var key in data.nodes) {
-            let node = data.nodes[key];
-            this.output(node, 1);
-            for (let i = 0; i < node.children.length; i++) {
-                let child = node.children[i];
-                if (child.type === "primitive") {
-                    console.log(
-                        new Array(2 * 4).join(" "),
-                        "-",
-                        child.type,
-                        "with",
-                        child.representations.length,
-                        child.subtype,
-                        "representation(s)"
-                    );
-                    if (child.subtype === "nurbs") {
-                        console.log(
-                            new Array(3 * 4).join(" "),
-                            "-",
-                            child.representations[0].controlpoints.length,
-                            "control points"
-                        );
-                    }
-                } else {
-                    this.output(child, 2);
-                }
+        /** @type {Record<string, THREE.Object3D>} */
+        this.nodes = {};
+        this.app.scene.add(this.generateSceneGraph(data));
+    }
+
+    /**
+     * @param {MySceneData} data
+     * @param {string} node
+     * @param {boolean} castShadow
+     * @param {boolean} receiveShadow
+     * @param {THREE.Material | undefined} material
+     *
+     * @returns {THREE.Object3D}
+     */
+    generateSceneGraph(
+        data,
+        node = data.rootId,
+        pCastShadow = false,
+        pReceiveShadow = false,
+        pMaterial = undefined
+    ) {
+        const nodeData = data.nodes[node];
+
+        const castShadow = nodeData.castShadow || pCastShadow;
+        const receiveShadow = nodeData.receiveShadow || pReceiveShadow;
+        const material = this.materials[nodeData.materialIds[0]] ?? pMaterial;
+
+        const group = new THREE.Group();
+
+        group.add(
+            ...nodeData.children.map((child) =>
+                this.generateNode(
+                    data,
+                    child,
+                    castShadow,
+                    receiveShadow,
+                    material
+                )
+            )
+        );
+
+        group.castShadow = castShadow;
+        group.receiveShadow = receiveShadow;
+
+        nodeData.transformations.forEach((transformation) => {
+            switch (transformation.type) {
+                case "T":
+                    group.translateX(transformation.translate[0]);
+                    group.translateY(transformation.translate[1]);
+                    group.translateZ(transformation.translate[2]);
+                    break;
+                case "R":
+                    group.rotateX(degreesToRadians(transformation.rotation[0]));
+                    group.rotateY(degreesToRadians(transformation.rotation[1]));
+                    group.rotateZ(degreesToRadians(transformation.rotation[2]));
+                    break;
+                case "S":
+                    group.scale.set(...transformation.scale);
+                    break;
+            }
+        });
+
+        return group;
+    }
+
+    /**
+     * @param {MySceneData} data
+     * @param {any} node
+     * @param {boolean} castShadow
+     * @param {boolean} receiveShadow
+     * @param {THREE.Material | undefined} material
+     *
+     * @returns {THREE.Object3D}
+     */
+    generateNode(data, node, castShadow, receiveShadow, material) {
+        switch (node.type) {
+            case "primitive":
+                return this.generatePrimitive(
+                    node,
+                    castShadow,
+                    receiveShadow,
+                    material
+                );
+            case "node":
+                return this.generateSceneGraph(
+                    data,
+                    node.id,
+                    castShadow,
+                    receiveShadow,
+                    material
+                );
+            case "pointlight": {
+                const light = new THREE.PointLight(
+                    node.color,
+                    node.intensity,
+                    node.distance,
+                    node.decay
+                );
+                light.position.set(...node.position);
+                light.castShadow = node.castShadow;
+                light.shadow.camera.far = node.shadowfar;
+                light.shadow.mapSize = node.shadowmapsize;
+                return light;
+            }
+            case "spotlight": {
+                const light = new THREE.SpotLight(
+                    node.color,
+                    node.intensity,
+                    node.distance,
+                    degreesToRadians(node.angle),
+                    node.penumbra,
+                    node.decay
+                );
+                light.position.set(...node.position);
+                light.target.position.set(...node.target);
+                light.castShadow = node.castShadow;
+                light.shadow.camera.far = node.shadowfar;
+                light.shadow.mapSize = node.shadowmapsize;
+                return light;
+            }
+            case "directionallight": {
+                const light = new THREE.DirectionalLight(
+                    node.color,
+                    node.intensity
+                );
+                light.position.set(...node.position);
+                light.castShadow = node.castShadow;
+                light.shadow.camera.left = node.shadowleft;
+                light.shadow.camera.right = node.shadowright;
+                light.shadow.camera.top = node.shadowtop;
+                light.shadow.camera.bottom = node.shadowbottom;
+                light.shadow.camera.far = node.shadowfar;
+                light.shadow.mapSize = node.shadowmapsize;
+                return light;
             }
         }
+
+        console.log(node.type);
+
+        return new THREE.Object3D();
+    }
+
+    /**
+     * @param {any} primitive
+     * @param {boolean} castShadow
+     * @param {boolean} receiveShadow
+     * @param {THREE.Material | undefined} material
+     *
+     * @returns {THREE.Object3D}
+     */
+    generatePrimitive(primitive, castShadow, receiveShadow, material) {
+        let mesh;
+
+        switch (primitive.subtype) {
+            case "rectangle": {
+                const representation = primitive.representations[0];
+
+                const x = (representation.xy2[0] + representation.xy1[0]) / 2;
+                const y = (representation.xy2[1] + representation.xy1[1]) / 2;
+
+                const width = representation.xy2[0] - representation.xy1[0];
+                const height = representation.xy2[1] - representation.xy1[1];
+
+                mesh = new THREE.Mesh(
+                    new THREE.PlaneGeometry(
+                        width,
+                        height,
+                        representation.parts_x,
+                        representation.parts_y
+                    ),
+                    material
+                );
+
+                mesh.position.set(x, y, 0);
+
+                break;
+            }
+            case "triangle": {
+                // TODO
+                break;
+            }
+            case "box": {
+                const representation = primitive.representations[0];
+
+                const x = (representation.xyz2[0] + representation.xyz1[0]) / 2;
+                const y = (representation.xyz2[1] + representation.xyz1[1]) / 2;
+                const z = (representation.xyz2[2] + representation.xyz1[2]) / 2;
+
+                const width = representation.xyz2[0] - representation.xyz1[0];
+                const height = representation.xyz2[1] - representation.xyz1[1];
+                const depth = representation.xyz2[2] - representation.xyz1[2];
+
+                mesh = new THREE.Mesh(
+                    new THREE.BoxGeometry(
+                        width,
+                        height,
+                        depth,
+                        representation.parts_x,
+                        representation.parts_y,
+                        representation.parts_z
+                    ),
+                    material
+                );
+
+                mesh.position.set(x, y, z);
+
+                break;
+            }
+            case "cylinder": {
+                const representation = primitive.representations[0];
+
+                mesh = new THREE.Mesh(
+                    new THREE.CylinderGeometry(
+                        representation.top,
+                        representation.base,
+                        representation.height,
+                        representation.slices,
+                        representation.stacks,
+                        !representation.capsclose,
+                        representation.thetaStart,
+                        representation.thetaLength
+                    ),
+                    material
+                );
+
+                break;
+            }
+            case "sphere": {
+                const representation = primitive.representations[0];
+
+                mesh = new THREE.Mesh(
+                    new THREE.SphereGeometry(
+                        representation.radius,
+                        representation.slices,
+                        representation.stacks,
+                        representation.phiStart,
+                        representation.phiLength,
+                        representation.thetaStart,
+                        representation.thetaLength
+                    ),
+                    material
+                );
+
+                break;
+            }
+        }
+
+        if (mesh) {
+            mesh.castShadow = castShadow;
+            mesh.receiveShadow = receiveShadow;
+        }
+
+        return mesh ?? new THREE.Object3D();
     }
 
     update() {}
