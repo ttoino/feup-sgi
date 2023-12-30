9f6ff128f05f9ad0e75bb5c9c1af764857698e9d
Nuno Pereira <nunoafonso2002@gmail.com>
2023-12-30 22:41:30 +0000

    Added track detection to vehicle

--- tp3/vehicles/VehicleController.js
+++ tp3/vehicles/VehicleController.js
@@ -1,3 +1,4 @@
+import * as THREE from "three";
 import { Game } from "../Game.js";
 import Vehicle, { MAX_SPEED } from "./Vehicle.js";
 
@@ -9,6 +10,8 @@ export default class VehicleController {
     constructor(game, vehicle) {
         this.game = game;
         this.vehicle = vehicle;
+
+        this.trackTracker = new THREE.Raycaster(this.vehicle.position.clone(), new THREE.Vector3(0, -1, 0));
     }
 
     installPlayerControls() {
@@ -90,5 +93,38 @@ export default class VehicleController {
      */
     update(delta) {
         this.vehicle.update(delta);
+
+        this.#checkOnTrack();
+    }
+
+    #checkOnTrack() {
+
+        const position = new THREE.Vector3().copy(this.vehicle.position.clone());
+        position.y = 1;
+
+        this.trackTracker.set(position, new THREE.Vector3(0, -1, 0));
+
+        const intersects = this.trackTracker.intersectObjects(this.game.contents.track.children);
+
+        if (intersects.length <= 0) {
+            // Out of track
+            if (this.previousMaxSpeed === undefined) {
+                this.vehicle.applyEffect((vehicle) => {
+                    this.previousMaxSpeed = vehicle.maxSpeed;
+                    vehicle.maxSpeed *= 0.7;
+                })
+            }
+
+        } else {
+            if (this.previousMaxSpeed !== undefined) {
+                // this is to make the types happy
+                const previousMaxSpeed = this.previousMaxSpeed;
+
+                this.vehicle.applyEffect((vehicle) => {
+                    vehicle.maxSpeed = previousMaxSpeed;
+                })
+                delete this.previousMaxSpeed;
+            }
+        }
     }
 }
